182bac7a4b5382abe882804f6001d3ca5fa21443
Mehmet Kalich <86363079+Mehmet-Kalich@users.noreply.github.com>
2022-02-08 21:13:51 +0000

    Add files via upload

---
+++ Back End - Lambda Functions/incrementViewCount.py
@@ -0,0 +1,38 @@
+import json, boto3
+
+client = boto3.client('dynamodb')
+TableName = 'cloud-resume'
+
+def lambda_handler(event, context):
+    
+    '''
+    data = client.get_item(
+        TableName='cloud-resume',
+        Key = {
+            'stat': {'S': 'view-count'}
+        }
+    )
+    '''
+    
+    #data['Item']['Quantity']['N'] = str(int(data['Item']['Quantity']['N']) + 1)
+    
+    response = client.update_item(
+        TableName='cloud-resume',
+        Key = {
+            'stat': {'S': 'view-count'}
+        },
+        UpdateExpression = 'ADD Quantity :inc',
+        ExpressionAttributeValues = {":inc" : {"N": "1"}},
+        ReturnValues = 'UPDATED_NEW'
+        )
+        
+    value = response['Attributes']['Quantity']['N']
+    
+    return {
+            'statusCode': 200,
+            'body': value,
+            "headers": 
+            {
+            "Access-Control-Allow-Origin" : "*"
+            }
+        }
